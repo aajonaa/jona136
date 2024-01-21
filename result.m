204506aaa03d24d5b05fd2cdb3066bb969ab5041
JonaWong <wj15909447527@gmail.com>
2024-01-22 01:10:13 +0800

    2.1vs1.9333vs1.9-SMBSA1vsSMBSAvsBSA

--- result.m
+++ result.m
@@ -1,6 +1,6 @@
 %% show result Sheet
 % Specify the file path and sheet name
-filePath = 'exp_result/SMBSA-01-22_00_28/SMBSA.xlsx'
+filePath = 'exp_result/SMBSA1-01-22_00_51/SMBSA1.xlsx'
 sheetName = 'result';  % Replace with the desired sheet name
 
 % Read the table from the Excel file
