60174d96f97d5efde9469ffc8171522d9bdd955b
JonaWong <wj15909447527@gmail.com>
2024-01-22 01:35:46 +0800

    1vs3vs1vs4-SMBSA1vsSMBSAvsBSAvsSMA

--- expSMA.m
+++ expSMA.m
@@ -25,7 +25,7 @@ MaxFEs=300000;
 % algorithmName = {'BSSMA_28', 'SHADE', 'SADE', 'gs_SMA_l4'};
 % algorithmName = {'WGWO', 'W2GWO', 'W3GWO', 'GWO'};
 % algorithmName = {'W3GWO_4', 'W3GWO_8', 'W3GWO_6', 'W3GWO_2'};
-algorithmName = {'SMBSA1', 'SMBSA', 'BSA'};
+algorithmName = {'SMBSA1', 'SMBSA', 'BSA', 'SMA'};
 
 
 % Flod=10;
