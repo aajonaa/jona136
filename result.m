018ee025f7946e7a6945b2a48f9afd1607d27bb7
JonaWong <wj15909447527@gmail.com>
2024-01-25 01:08:58 +0800

    BSA_map8 finished

--- result.m
+++ result.m
@@ -1,6 +1,6 @@
 %% show result Sheet
 % Specify the file path and sheet name
-filePath = 'exp_result/BSA_map8-01-25_00_09/BSA_map8.xlsx'
+filePath = 'exp_result/BSA_map8-01-25_00_22/BSA_map8.xlsx'
 sheetName = 'result';  % Replace with the desired sheet name
 
 % Read the table from the Excel file
