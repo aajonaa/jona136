40f60218afdff13bd5ad618a976c5eb66fb7531c
JonaWong <wj15909447527@gmail.com>
2024-02-06 14:16:43 +0800

    update IBSA for balance and diversity

---
+++ BSAForBD/BSA.asv
@@ -0,0 +1,109 @@
+% Backtracking Search Optimization Algorithm (BSA) modified by Jona
+% 2024-1-23.
+% 2024-2-5 for balance and diversity.
+% function [best_pos,Convergence_curve] = BSA(N, Max_FEs, lb, ub, dim, fobj)
+function [average_distance,Div,best_pos,Convergence_curve]=BSA(N,Max_FEs,lb,ub,dim,fobj)
+
+
+    tic
+    disp('BSA tackle your problem');
+    
+    %%
+    Div=[];
+    average_distance=[];
+    iter=1;  %Number of iterations
+    %%
+    
+    %INITIALIZATION
+    DIM_RATE = 1;  
+    Convergence_curve = [];
+    FEs = 0;
+    bestFitness = inf;
+    best_pos = zeros(1,dim);
+    
+    AllFitness = inf * ones(1, N);
+    fitnessoffsprings = inf * ones(1, N);
+    if numel(lb)==1, lb=lb*ones(1,dim); ub=ub*ones(1,dim); end
+    X=initialization(N,dim,ub,lb); 
+    historical_X = initialization(N, dim, ub, lb);
+%     iter = 1; ##########
+
+    %% Add the sentence after X is defined
+    D= pdist(X,'euclidean');
+    average_distance(iter)=(sum(D(1:end)))/(N*(N-1)/2);
+    Div(iter)=Divergence_calculation(X,dim,N);
+    %%
+
+    for i = 1 : N
+        AllFitness(i) = fobj(X(i, :));
+        FEs  = FEs +1;
+    end
+    
+    while FEs < Max_FEs
+    
+        %SELECTION-I
+        if rand<rand, historical_X=X; end  
+        historical_X=historical_X(randperm(N),:); 
+        F=get_scale_factor; 
+        map=zeros(N,dim); 
+        if rand<rand
+            for i=1:N,  u=randperm(dim); map(i,u(1:ceil(DIM_RATE*rand*dim)))=1; end
+        else
+            for i=1:N,  map(i,randi(dim))=1; end
+        end
+    
+        % RECOMBINATION (MUTATION+CROSSOVER)   
+        offsprings=X + F * map .* (historical_X - X);   
+        offsprings=BoundaryControl(offsprings,lb,ub); 
+    
+        % SELECTON-II
+        for i = 1:N
+            fitnessoffsprings(i) = fobj(offsprings(i,:));
+            FEs = FEs + 1;
+        end
+        ind=fitnessoffsprings<AllFitness;
+        AllFitness(ind)=fitnessoffsprings(ind);
+        X(ind,:)=offsprings(ind,:);
+        [minimum_fitness, ind]=min(AllFitness);    
+        best_pos=X(ind,:);
+        bestFitness = minimum_fitness;
+        
+        %% Add the sentence before plot the dot.
+        D= pdist(X,'euclidean');
+        average_distance(iter)=(sum(D(1:end)))/(N*(N-1)/2);
+        Div(iter)=Divergence_calculation(X,dim,N);
+        %%
+    
+        Convergence_curve(iter)=bestFitness;
+        iter =iter + 1;
+    end
+    toc
+end
+
+function X=BoundaryControl(X,low,up)
+    [N,dim]=size(X);
+    for i=1:N
+        for j=1:dim                
+            k=rand<rand; 
+            if X(i,j)<low(j)
+                if k, X(i,j)=low(j); 
+                else X(i,j)=rand*(up(j)-low(j))+low(j); 
+                end 
+            end        
+            if X(i,j)>up(j)
+                if k, X(i,j)=up(j);  
+                else
+                    X(i,j)=rand*(up(j)-low(j))+low(j); 
+                end 
+            end
+        end
+    end
+end
+
+function F=get_scale_factor % you can change generation strategy of scale-factor,F    
+     F=3*randn; % STANDARD brownian-walk
+    % F=4*randg;  % brownian-walk    
+    % F=lognrnd(rand,5*rand);  % brownian-walk              
+    % F=1/normrnd(0,5);        % pseudo-stable walk (levy-like)
+    % F=1./gamrnd(1,0.5);      % pseudo-stable walk (levy-like, simulates inverse gamma distribution; levy-distiribution)   
+end
---
+++ BSAForBD/IBSA.m
@@ -0,0 +1,198 @@
+% Backtracking Search Optimization Algorithm (BSA) modified by Jona 2024-1-25.
+% 2024-2-5 for balance and diversity.
+
+% This version is the same with BSA_map8.m but delete the commented code.
+% This is the copy of the modBSA_e.
+
+% First change the function header. I
+% Second change the fitness calculation method and coment the FEs. II
+% Third change all the population be binary version by using TF function or bool operation. III
+% Forth cancel the boundary control. IV
+
+% I
+% function [best_pos,Convergence_curve] = IBSA(N, Max_FEs, lb, ub, dim, fobj)
+
+function [average_distance,Div,best_pos,Convergence_curve]=IBSA(N,Max_FEs,lb,ub,dim,fobj)
+
+% function [bestFitness, best_pos, Convergence_curve, Time] = bmodBSA(N, Max_FEs, dim, A, trn, vald, TFid, classifierFhd)
+
+    tic
+    disp('BSA tackle your problem');
+    
+    %%
+    Div=[];
+    average_distance=[];
+    iter=1;  %Number of iterations
+    %%
+    
+    %INITIALIZATION
+
+    bestFitness = inf;
+    pre_bestFitness = inf;
+
+    best_pos = zeros(1,dim);
+
+    AllFitness = inf * ones(1, N);
+    fitnessoffsprings = inf * ones(1, N);
+
+    % lb = 0;
+    % ub = 1;
+
+    if numel(lb)==1, lb=lb*ones(1,dim); ub=ub*ones(1,dim); end
+
+    % III
+    X=initialization(N,dim,ub,lb); 
+    % X=initialization(N,dim,ub,lb) > 0.5;
+    historical_X = initialization(N, dim, ub, lb);    
+    % historical_X = initialization(N, dim, ub, lb) > 0.5;
+    
+    %% Add the sentence after X is defined
+    D= pdist(X,'euclidean');
+    average_distance(iter)=(sum(D(1:end)))/(N*(N-1)/2);
+    Div(iter)=Divergence_calculation(X,dim,N);
+    %%
+
+    weight = ones(N, dim);
+
+    % II
+%     iter = 1; ##########
+    FEs = 0;
+    Convergence_curve = [];
+
+    %% parameter
+    count = 0;
+    c = 0.02;
+    d = 50;
+    z = 0.2;
+    flag = 1;
+
+    for i = 1 : N
+        % II
+        AllFitness(i) = fobj(X(i, :));
+        % AllFitness(i) = AccSz2(X(i,:), A, trn, vald, classifierFhd);
+        FEs  = FEs +1;
+    end
+    
+    %% Main loop
+    while FEs < Max_FEs
+
+        %% Calculate count and sort the fitness
+        [SmellOrder, SmellIndex] = sort(AllFitness);
+        bestFitness = SmellOrder(1);
+        if bestFitness < pre_bestFitness
+            count = count / 2;
+        else
+            count = count + 1;
+        end
+%         count
+        pre_bestFitness = bestFitness;
+        best_pos = X(SmellIndex(1), :);
+    
+        %% SELECTION-I
+        if flag == 0, historical_X = X; end
+        historical_X=historical_X(randperm(N),:); 
+
+        %% RECOMBINATION (MUTATION+CROSSOVER)   
+        F=get_scale_factor; 
+        map=zeros(N,dim); 
+        p = 1/(1+exp(-c * (count - d)));
+        p = 1.3 * p - 0.3;
+        if p > z
+            flag = 0;
+            b = 1 - FEs/Max_FEs;
+            vc = unifrnd(-b, b, 1, N);
+            for i = 1:N
+                if rand > (FEs/Max_FEs)^3
+                    vc(i) = 1;
+                end
+            end
+            for i = 1:N
+                if SmellIndex(i) > 1
+                    u = randperm(dim);
+                    map(i, u(1: ceil(rand * dim))) = 1;
+                    Mutant(i, :) = vc(i) * X(i, :) + F * map(i, :) .* (historical_X(i, :) - X(i, :));
+                    % III
+                    % for j = 1:dim
+                    %     temp = Mutant(i, j);
+                    %     Mutant(i, j) = transferFun(Mutant(i, j), temp, TFid);
+                    % end
+                else
+                    Mutant(i, :) = best_pos + F * (historical_X(i, :) - X(i, :)); %% F1-F4
+                    % III
+                    % for j = 1:dim
+                    %     temp = Mutant(i, j);
+                    %     Mutant(i, j) = transferFun(Mutant(i, j), temp, TFid);
+                    % end
+                end
+            end
+        else
+            flag = 1;
+            for i = 1:N
+                map(i, randi(dim)) = 1;
+                Mutant(i, :) = X(i, :) + F * map(i, :) .* (historical_X(i, :) - X(i, :));
+                % III
+                % for j = 1:dim
+                %     temp = Mutant(i, j);
+                %     Mutant(i, j) = transferFun(Mutant(i, j), temp, TFid);
+                % end
+            end
+        end 
+        offsprings = Mutant;
+        % IV
+        offsprings=BoundaryControl(offsprings,lb,ub); 
+    
+        %% SELECTON-II
+        for i = 1:N
+            % II
+            fitnessoffsprings(i) = fobj(offsprings(i,:));
+            % fitnessoffsprings(i) = AccSz2(offsprings(i,:), A, trn, vald, classifierFhd);
+            FEs = FEs + 1;
+        end
+        ind=fitnessoffsprings<AllFitness;
+        AllFitness(ind)=fitnessoffsprings(ind);
+        X(ind,:)=offsprings(ind,:);
+        [minimum_fitness, ind]=min(AllFitness);    
+        best_pos=X(ind,:);
+        Destination_fitness = minimum_fitness;
+    
+        %% Add the sentence before plot the dot.
+        D= pdist(X,'euclidean');
+        average_distance(iter)=(sum(D(1:end)))/(N*(N-1)/2);
+        Div(iter)=Divergence_calculation(X,dim,N);
+        %%
+        
+        Convergence_curve(iter) = Destination_fitness;
+        iter =iter + 1;
+    end
+    toc
+    % Time = toc;
+end
+    
+% IV
+function X=BoundaryControl(X,low,up)
+    [N,dim]=size(X);
+    for i=1:N
+        for j=1:dim                
+            k=rand<rand; 
+            if X(i,j)<low(j)
+                if k, X(i,j)=low(j); 
+                else X(i,j)=rand*(up(j)-low(j))+low(j); 
+                end 
+            end        
+            if X(i,j)>up(j)
+                if k, X(i,j)=up(j);  
+                else
+                    X(i,j)=rand*(up(j)-low(j))+low(j); 
+                end 
+            end
+        end
+    end
+end
+    
+function F=get_scale_factor % you can change generation strategy of scale-factor,F    
+        F=3*randn; % STANDARD brownian-walk
+    % F=4*randg;  % brownian-walk    
+    % F=lognrnd(rand,5*rand);  % brownian-walk              
+    % F=1/normrnd(0,5);        % pseudo-stable walk (levy-like)
+    % F=1./gamrnd(1,0.5);      % pseudo-stable walk (levy-like, simulates inverse gamma distribution; levy-distiribution)   
+end
