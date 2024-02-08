914628221dab02d155cf64615b32d4366311a291
JonaWong <wj15909447527@gmail.com>
2024-02-09 03:05:24 +0800

    result show the sqrt(count) is better

--- result.m
+++ result.m
@@ -1,6 +1,6 @@
 %% show result Sheet
 % Specify the file path and sheet name
-filePath = 'exp_result/modBSA_revise-02-09_02_38/modBSA_revise.xlsx'
+filePath = 'exp_result/modBSA_revise-02-09_02_52/modBSA_revise.xlsx'
 sheetName = 'result';  % Replace with the desired sheet name
 
 % Read the table from the Excel file
